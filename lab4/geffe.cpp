e78e7e58fa95031b08b29c5812cba05eb89bc53d
Karina Malij <karinamalij0303@gmail.com>
2026-05-22 16:48:40 +0300

    2

--- lab4/geffe.cpp
+++ lab4/geffe.cpp
@@ -1,23 +1,24 @@
 #include "geffe.h"
 
-Geffe::Geffe(LFSR l1, LFSR l2, LFSR l3): L1(l1), L2(l2), L3(l3) {
+Geffe::Geffe(const LFSR& r1,  const LFSR& r2,const LFSR& r3): l1(r1), l2(r2), l3(r3)
+{ }
 
-}
+int Geffe::step() {
+    int x = l1.step();
+    int y = l2.step();
+    int s = l3.step();
 
-vector<int> Geffe::generate(int n) {
-    vector<int> z;
-    z.reserve(n);
-
-    for (int i = 0; i < n; i++) {
-        int x = L1.step();
-        int y = L2.step();
-        int s = L3.step();
-
-
-        int zi = (s == 1) ? x : y;
-
-        z.push_back(zi);
-    }
+    int z = (s & x) ^ ((1 ^ s) & y);
 
     return z;
+}
+
+vector<int> Geffe::generate(int length) {
+    vector<int> sequence;
+
+    for (int i = 0; i < length; i++) {
+        sequence.push_back(step());
+    }
+
+    return sequence;
 }
--- lab4/geffe.h
+++ lab4/geffe.h
@@ -1,17 +1,19 @@
 #pragma once
-#include <vector>
 #include "lfsr.h"
+#include <vector>
 
 using namespace std;
 
 class Geffe {
 private:
-    LFSR L1;
-    LFSR L2;
-    LFSR L3;
+    LFSR l1;
+    LFSR l2;
+    LFSR l3;
 
 public:
-    Geffe(LFSR l1, LFSR l2, LFSR l3);
+    Geffe(const LFSR& r1, const LFSR& r2, const LFSR& r3);
 
-    vector<int> generate(int n);
+    int step();
+
+    vector<int> generate(int length);
 };
