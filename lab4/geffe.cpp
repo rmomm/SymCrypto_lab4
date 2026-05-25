80f5b0bb7f2e81d4c19f50b697b9d041c39f88e4
Karina Malij <karinamalij0303@gmail.com>
2026-05-25 13:26:17 +0300

    cc

--- lab4/geffe.cpp
+++ lab4/geffe.cpp
@@ -1,24 +1,17 @@
 #include "geffe.h"
 
-Geffe::Geffe(const LFSR& r1,  const LFSR& r2,const LFSR& r3): l1(r1), l2(r2), l3(r3)
-{ }
+Geffe::Geffe(const LFSR& r1,const LFSR& r2, const LFSR& r3) : l1(r1), l2(r2), l3(r3)
+{}
 
-int Geffe::step() {
-    int x = l1.step();
-    int y = l2.step();
-    int s = l3.step();
-
-    int z = (s & x) ^ ((1 ^ s) & y);
-
-    return z;
-}
-
-vector<int> Geffe::generate(int length) {
-    vector<int> sequence;
+vector<uint8_t> Geffe::generate(uint32_t f1, uint32_t f2, uint32_t f3,int length) {
+    vector<uint8_t> x =  l1.gen(f1, length);
+    vector<uint8_t> y = l2.gen(f2, length);
+    vector<uint8_t> s = l3.gen(f3, length);
+    vector<uint8_t> z(length);
 
     for (int i = 0; i < length; i++) {
-        sequence.push_back(step());
+        z[i] = (s[i] & x[i]) ^ ((1 ^ s[i]) & y[i]);
     }
 
-    return sequence;
+    return z;
 }
--- lab4/geffe.h
+++ lab4/geffe.h
@@ -1,19 +1,18 @@
 #pragma once
+
 #include "lfsr.h"
-#include <vector>
 
-using namespace std;
-
-class Geffe {
+class Geffe
+{
 private:
+
     LFSR l1;
     LFSR l2;
     LFSR l3;
 
 public:
+
     Geffe(const LFSR& r1, const LFSR& r2, const LFSR& r3);
 
-    int step();
-
-    vector<int> generate(int length);
+    vector<uint8_t> generate(uint32_t f1, uint32_t f2, uint32_t f3, int length);
 };
